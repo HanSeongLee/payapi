c11a6630f84b962c6ee65e5efb3d99d924e02bd3
HanSeong Lee <jkhanseong@gmail.com>
2022-02-21 19:34:45 +0900

    ADD | Logo

---
+++ components/Logo/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from 'next/link';
+import LogoImage from '/public/img/logo.svg';
+import LogoInvertedImage from '/public/img/logo-invert.svg';
+
+type IProps = {
+    invert?: boolean;
+};
+
+const Logo: React.FC<IProps> = ({ invert }) => {
+    return (
+        <Link href={'/'}>
+            <a>
+                {!invert ? (<LogoImage />) : (<LogoInvertedImage />)}
+            </a>
+        </Link>
+    );
+};
+
+Logo.defaultProps = {
+    invert: false,
+};
+
+export default Logo;
