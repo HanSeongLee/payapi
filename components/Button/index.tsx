24b4d9d5228b8f6055934585dc63af141d5448f1
HanSeong Lee <jkhanseong@gmail.com>
2022-02-22 19:25:18 +0900

    ADD | Change attributes of button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,7 +1,7 @@
-import React, {HTMLAttributes} from "react";
+import React, {ButtonHTMLAttributes} from "react";
 import styles from './style.module.scss';
 
-const Button: React.FC<HTMLAttributes<HTMLButtonElement>> = ({ children, ...props }) => {
+const Button: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({ children, ...props }) => {
     return (
         <button className={styles.button}
                 {...props}
