2506604d453316b5424483e61650b0c2de65b973
HanSeong Lee <jkhanseong@gmail.com>
2022-02-22 20:01:56 +0900

    ADD | Partners section

---
+++ components/PartnersSection/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from './style.module.scss';
+import Container from "../Container";
+import PartnerContainer from "../../containers/PartnerContainer";
+import Button from "../Button";
+
+const PartnersSection: React.FC = () => {
+    return (
+        <section className={styles.partnersSection}>
+            <Container className={styles.container}>
+                <PartnerContainer className={styles.partnerContainer} />
+
+                <h2 className={styles.title}>
+                    Who we work with
+                </h2>
+                <p className={styles.description}>
+                    Today, millions of people around the world have successfully connected their accounts to apps they love using our API. We provide developers with the tools they need to create easy and accessible experiences for their users.
+                </p>
+
+                <Button variants={'outline'}>
+                    About Us
+                </Button>
+            </Container>
+        </section>
+    );
+};
+
+export default PartnersSection;
