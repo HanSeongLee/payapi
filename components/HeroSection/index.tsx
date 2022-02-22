60a717a35097eff0cd2c950ef9a493524ec05434
HanSeong Lee <jkhanseong@gmail.com>
2022-02-22 19:27:49 +0900

    ADD | Hero section

---
+++ components/HeroSection/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from './style.module.scss';
+import HeroImage from '/public/img/home/desktop/illustration-phone-mockup.svg'
+import Link from 'next/link';
+import ScheduleDemoFormContainer from "../../containers/ScheduleDemoFormContainer";
+import Container from "../Container";
+
+const HeroSection: React.FC = () => {
+    return (
+        <section className={styles.heroSection}>
+            <Container className={styles.container}>
+                <div className={styles.heroImageWrapper}>
+                    <HeroImage className={styles.heroImage}/>
+                </div>
+                <h2 className={styles.title}>
+                    Start building with our APIs for absolutely free.
+                </h2>
+                <ScheduleDemoFormContainer className={styles.scheduleDemoContainer}/>
+                <p className={styles.questions}>
+                    Have any questions? <Link href={'/contact'}><a>Contact Us</a></Link>
+                </p>
+            </Container>
+        </section>
+    );
+};
+
+export default HeroSection;
