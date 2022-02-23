33bf3c418dfdcbe5384ad3cce21b749cc4cfa48c
HanSeong Lee <jkhanseong@gmail.com>
2022-02-23 19:17:36 +0900

    ADD | Feature card

---
+++ components/FeatureCard/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from './style.module.scss';
+import cn from "classnames";
+
+type IProps = {
+    src: string;
+    title: string;
+    description: string;
+    size?: 'normal' | 'large',
+};
+
+const FeatureCard: React.FC<IProps> = ({ src, title, description, size }) => {
+    return (
+        <div className={cn(styles.featureCard, {
+            [styles.large]: size === 'large',
+        })}>
+            <img className={styles.thumbnail}
+                 src={src}
+                 alt={''}
+            />
+            <h2 className={styles.title}>
+                {title}
+            </h2>
+            <p className={styles.description}>
+                {description}
+            </p>
+        </div>
+    );
+};
+
+FeatureCard.defaultProps = {
+    size: 'normal',
+};
+
+export default FeatureCard;
