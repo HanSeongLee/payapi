c224023e680483c3895fc4554a9972d31e10bffb
HanSeong Lee <jkhanseong@gmail.com>
2022-02-27 18:15:14 +0900

    ADD | Contact section

---
+++ components/ContactSection/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./style.module.scss";
+import Container from "../Container";
+import TitledScheduleDemoForm from "../TitledScheduleDemoForm";
+import PartnerContainer from "../../containers/PartnerContainer";
+import ContactForm from "../ContactForm";
+import ContactFormContainer from "../../containers/ContactFormContainer";
+
+const ContactSection: React.FC = () => {
+    return (
+        <section className={styles.contactSection}>
+            <Container className={styles.container}>
+                <h2 className={styles.title}>
+                    Submit a help request and we’ll get in touch shortly.
+                </h2>
+
+                <ContactFormContainer />
+
+                <div className={styles.partnerWrapper}>
+                    <h3 className={styles.title}>
+                        Join the thousands of innovators already building with us
+                    </h3>
+                    <PartnerContainer className={styles.partnerContainer}/>
+                </div>
+
+                <TitledScheduleDemoForm/>
+            </Container>
+        </section>
+    );
+};
+
+export default ContactSection;
