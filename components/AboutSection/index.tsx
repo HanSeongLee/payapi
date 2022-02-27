26407e01343491d44ad03278a276aa35fe7a19bc
HanSeong Lee <jkhanseong@gmail.com>
2022-02-27 19:42:35 +0900

    ADD | About section on tablet

--- components/AboutSection/index.tsx
+++ components/AboutSection/index.tsx
@@ -7,7 +7,7 @@ import CompanySummary from "../CompanySummary";
 
 const AboutSection: React.FC = () => {
     return (
-        <section className={styles.pricingSection}>
+        <section className={styles.aboutSection}>
             <Container className={styles.container}>
                 <h2 className={styles.title}>
                     We empower innovators<br/>
@@ -15,24 +15,34 @@ const AboutSection: React.FC = () => {
                 </h2>
 
                 <DescriptionCard title={'Our Vision'}
-                                description={'Our main goal is to build beautiful consumer experiences along with developer-friendly infrastructure. The result is an intelligent tool that gives everyone the ability to create amazing products that solve big problems. We are deeply focused on democratizing financial services through technology.'}
+                                 description={'Our main goal is to build beautiful consumer experiences along with developer-friendly infrastructure. The result is an intelligent tool that gives everyone the ability to create amazing products that solve big problems. We are deeply focused on democratizing financial services through technology.'}
                 />
                 <DescriptionCard title={'Our Business'}
-                                description={'At the core of our platform is the technical infrastructure APIs that connect consumers. Our innovative product provides key insights for businesses and individuals, as well as robust reporting for traditional financial institutions and developers.'}
+                                 description={'At the core of our platform is the technical infrastructure APIs that connect consumers. Our innovative product provides key insights for businesses and individuals, as well as robust reporting for traditional financial institutions and developers.'}
                 />
+            </Container>
 
-                <img className={styles.thumbnail}
-                     src={'/img/about/mobile/image-team-members.jpg'}
-                     alt={'Team members'}
-                />
+            <Container className={styles.thumbnailContainer}>
+                <picture>
+                    <source srcSet={'/img/about/tablet/image-team-members.jpg'}
+                            media={'(min-width: 767px)'}
+                    />
 
-                <CompanySummary />
+                    <img className={styles.thumbnail}
+                         src={'/img/about/mobile/image-team-members.jpg'}
+                         alt={'Team members'}
+                    />
+                </picture>
+            </Container>
+
+            <Container className={styles.container}>
+                <CompanySummary/>
 
                 <DescriptionCard title={'The Culture'}
-                                description={'We strongly believe there\'s always an opportunity to learn from each other outside of day-to-day work, whether it\'s company-wide offsites, internal hackathons, or co-worker meetups. We always value cross-team collaboration and diversity of thought, no matter the job title.'}
+                                 description={'We strongly believe there\'s always an opportunity to learn from each other outside of day-to-day work, whether it\'s company-wide offsites, internal hackathons, or co-worker meetups. We always value cross-team collaboration and diversity of thought, no matter the job title.'}
                 />
                 <DescriptionCard title={'The People'}
-                                description={' We\'re all passionate about building a more efficient and inclusive financial infrastructure together. At PayAPI, we have diverse backgrounds and skills.'}
+                                 description={' We\'re all passionate about building a more efficient and inclusive financial infrastructure together. At PayAPI, we have diverse backgrounds and skills.'}
                 />
 
                 <TitledScheduleDemoForm/>
