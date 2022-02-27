17449a8c6230615137c6e56bbc96f0adc8e26fc4
HanSeong Lee <jkhanseong@gmail.com>
2022-02-27 19:05:50 +0900

    ADD | Feature section on tablet

--- components/FeatureSection/index.tsx
+++ components/FeatureSection/index.tsx
@@ -19,20 +19,22 @@ const FeatureSection: React.FC = () => {
                              size={'large'}
                 />
 
-                <FeatureCard src={'/img/home/desktop/icon-personal-finances.svg'}
-                             title={'Personal Finances'}
-                             description={'Consolidate financial data from multiple sources and categorize transactions up to 2 years of history. Analyze reports to reconcile activities in your account.'}
-                />
-                <FeatureCard src={'/img/home/desktop/icon-banking-and-coverage.svg'}
-                             title={'Banking & Coverage'}
-                             description={'With our platform, you can speed up account onboarding and support ongoing payments for checking, savings, credit card, and brokerage accounts.'}
-                />
-                <FeatureCard src={'/img/home/desktop/icon-consumer-payments.svg'}
-                             title={'Consumer Payments'}
-                             description={'It’s easier to set up secure bank payments with us through a flow designed with the user experience in mind. Customers could instantly authenticate their account.'}
-                />
+                <div className={styles.featureContainer}>
+                    <FeatureCard src={'/img/home/desktop/icon-personal-finances.svg'}
+                                 title={'Personal Finances'}
+                                 description={'Consolidate financial data from multiple sources and categorize transactions up to 2 years of history. Analyze reports to reconcile activities in your account.'}
+                    />
+                    <FeatureCard src={'/img/home/desktop/icon-banking-and-coverage.svg'}
+                                 title={'Banking & Coverage'}
+                                 description={'With our platform, you can speed up account onboarding and support ongoing payments for checking, savings, credit card, and brokerage accounts.'}
+                    />
+                    <FeatureCard src={'/img/home/desktop/icon-consumer-payments.svg'}
+                                 title={'Consumer Payments'}
+                                 description={'It’s easier to set up secure bank payments with us through a flow designed with the user experience in mind. Customers could instantly authenticate their account.'}
+                    />
+                </div>
 
-                <TitledScheduleDemoForm />
+                <TitledScheduleDemoForm/>
             </Container>
         </section>
     );
